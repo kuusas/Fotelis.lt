f2b0859f53a222d9233e4397f8d9dd3cff7d39a4
Zilvinas Kuusas <zilvinas@kuusas.lt>
2013-04-21 12:07:53 +0300

    webconf post

---
+++ app/posts/reikia-it-pros-konferencijos-lietuvoje/autoload.php
@@ -0,0 +1,11 @@
+<?php
+$this->add(__DIR__, array(
+    'name' => 'Reikia IT pros konferencijos Lietuvoje',
+    'shortText' => 'Šeštadienį vyko Infoshow rengiama web konferencija. Kadaise tai buvo tik PHP konferencija, bet su laiku išaugo. Aš, jau berods treti metai išeilės (o gal ir ketvirti), ateinu pasiklausyti. Pranešimai visuomet nenuvilia. Bent jau vienas visada būna, kuris gelbėja padėtį ir netenka gailėtis ėjus į renginį.
+Bet kas šiemet nuliūdino - konferencijos organizavimas ir tai kiek žmonių tesusirinko...',
+    'date' => '2013-04-21',
+    'author' => 'kuusas',
+    'category' => 'bendrai',
+    'contentPath' => __DIR__ . '/post.php',
+    'path' => __DIR__,
+));
---
+++ app/posts/reikia-it-pros-konferencijos-lietuvoje/post.php
@@ -0,0 +1,45 @@
+<h2>Infoshow web konferencija</h2>
+
+<p>Šeštadienį vyko Infoshow rengiama web konferencija. Kadaise tai buvo tik PHP konferencija, bet su laiku išaugo. Aš, jau berods treti metai išeilės (o gal ir ketvirti), ateinu pasiklausyti. Pranešimai visuomet nenuvilia. Bent jau vienas visada būna, kuris gelbėja padėtį ir netenka gailėtis ėjus į renginį.<br />
+Bet kas šiemet nuliūdino - konferencijos organizavimas ir tai kiek žmonių tesusirinko. Būtent susirinkusiųjų skaičius ir atspindėjo organizacines problemas, nes pranešimų buvo tikrai rimtų ir vertų dėmesio bet kokio lygio specialistams. Tikrai nelaikyčiau šios, ar bet kurios kitos Infosa organizuotos, web konferencijos studentiška. Pranešimai - super. Bet kaip reikėjo sužinoti apie šią konferenciją? Nežinia… Aš pats konferencijos nepraleidau tik dėl to, kad draugas užsiminė apie tai, ir dar kelis kartus priminė. Internete - jokios reklamos, bendruomenėse - jokios reklamos, socialiniuose tinkluose - jokios reklamos. Net paprasčiausio Facebook evento nebuvo sukurta. Kodėl? Kokiu tikslu ši konferencija tuomet buvo organizuojama?<br />
+Antra organizacinė problema - pranešėjai ir konferencijos programa keitėsi iki paskutinės akimirkos. Vienu metu buvo suplanuoti pranešimai iki 20:30. Kas tiek laiko (nuo 11 val. ryto) gali skirti tiek laiko? O galiausiai, paskutinę dieną, prieš pat konferenciją, programą perdėliojo, turėjo trukti viskas iki ~18:00, bet pietums palikta 30min. Really? Ne visi tas picas vietoj nori valgyt.<br />
+Ir galiausiai - paskutinio pranešimo net nebuvo. Kur dingo pranešėjas? Ar jam tiesiog nebuvo leista pasisakyt?<br />
+Reikėjo padejuot. Kitais metais organizatoriam tiesiog užteks linką nusiųst į šį blogo postą.</p>
+
+<p>Geroji dalis. Pranešimais likau patenkintas. Ne visų pranešėjų sarkazmą perpratau, ne visi pranešimai man aktualūs buvo, bet visi - pasiruošę ir šnekantys apie tai, ką išmano. Galima teigti, kad konferencija - profesionalų.<br />
+Šiais metais, kaip ir praeitais, labiausiai patiko Sergejaus Barinovo pranešimas. Nors iš pradžių bijojau, kad evangelizuos .NET environmentus ir toolsus, bet prasidėjus pranešimui iškarto užkabino. Supratau, kad išankstinis nusistatymas buvo neteisingas, o pats pranešimas privertė susimąstyti turbūt betkurį developerį, bent jau apie deploymento automatizavimą.</p>
+
+<h2>Konferencijos Lietuvoje</h2>
+
+<p>Infoshow web konferencija - berods vienintelė konferencija Lietuvoje, į kurią susirenka įvairaus plauko developeriai. LOGIN - visai kito pobūdžio. Su meetupais atrodo ledai šiek tiek pajudėjo. Vyksta mėnesniai Vilniaus PHP bendruomenės susitikimai, karts nuo karto įvykstas ir Kauno PHP developerių meetupas, vyksta kažkas JS, Python, Ruby, Java ir .NET pasauliuose. Sakau kažkas - nes neesu artimai pažįstamas su jais. Bet visi taip ir lieka atsiriboję vieni nuo kitų. Įdomi idėja Code-Hat community - suburti įvairių sričių developerius, bet - neaišku ar tai idėja bus ilgai gyva, neaišku už ko ji kabinasi, turbūt tik NFQ ratelyje ji gyvuoja, nes pasauliui gyvybės ženklų nerodo. O gal tas įvykęs vienas susitikimas buvo vienintelis pabandymas, tuo viskas ir pasibaigė?<br />
+Kas bebūtų su meetupais, bet konferencijų Lietuvoje IT srityje jaučiasi trūkumas. Šio klausimu nebūsiu dejuojantis "kaip čia blogai, kad nieka neorganizuoja" - reikia organizuoti patiem!</p>
+
+<h2>Organizuojam profesionalų konferenciją!</h2>
+
+<p>Pradinė idėja - summer hacker campas, nestandartinio formato IT konferencija, geekų sambūris, dar neaišku kaip tokį renginį pavadint. Norėtume suorganizuoti nestandartinio formato konferenciją, kurioje žmogus-geekas nebūtų sukaustytas formalios aplinkos, formaliai atrodančių žmonių. Norėtume pritraukti įvairių sričių specialistų, įvairių programavimo kalbų atstovų, skatintinti brendravimą tarpusavy, dalintis patirtimi ir žiniomis, įdomiais ar keistais sprendimais ir gautais rezultatais. O gal kas norėtų pristatyti problemą ir ją bandyti kartu išspręsti? Galbūt netikėtai PHP developeriui sprendimą pasiūlytų Python specialistas, nes jo kalboje tai yra seniausiai išspręsta ar panašiai. Galbūt kas norėtų papasakoti plačiau apie Arduino ar Raspberry Pi? Scala ar Haskell? Objective-C ir Cocoa? Projektų valdymo procesus? O gal kokį workshopą padaryt? O galbūt kuris geekas norėtų papasakot apie savo visiškai negeekišką patirtį? Pvz. kaip iškūrent pirtį ar nubėgt pusmaratonį?<br />
+Nuomojam sodybą ar apsistojam kokiam kempinge, arba tiesiog važiuojam kažkur prie ežero su palapinėm. Išsinuomojam generatorių, susiveikiam internetą - ir jau turim pradinę infrastruktūrą.<br />
+Norėtūsi, kad ir ateinantys žmonės nebūtų vien žiūrovai, norėtų bendrauti, galbūt paruošti mini pranešimus.</p>
+
+<p>Kad ir kaip bebūtų - reikia kažko, dėl ko žmonės eitų į konferenciją ir žinoma, kad būtų galima tai vadinti konferencija (nes šiaip pagert - žmonos gali nelesiti) - reikia pranešimų. Pirmas motyvas eiti - pranešimai. Pranešimų reikia vertų dėmesio, tad viena iš užduočių būtų rasti gerus pranešėjus, kurie garantuotų kokybiškus pranešimus.<br />
+Antras motyvas eiti - smagiai praleisti laiką, pailsėti. Reikia sukurti atmosferą ir aplinką, kad tai nekeltų asociacijų su darbu, pareigom ir panašiai, kad visi galėtų atsipalaiduoti.</p>
+
+<p><strong>Vieta.</strong> Kaip ir jau užsiminiau - vieta turbūt būtų koks kempingas arba sodyba su galimybe statysi palapines kieme. Kam reikia komforto - gautų lovą ar čiužinį viduje, kas labiau prie gamtos - palapinėj vietą. Ar pasiimti antras puses? Klausimas.<br />
+Priklausomai nuo vietos - elektra būtų pasirūpinta, vistik kaip be kompo. Dėl interneto - dar irgi galima pagalvot. Ar jo tikrai reikia? Gal užtektų vietoj pasikurti serverį ir duoti kiekvienam po virtualią mašiną eksperimentam? Bet daug kas savo įrankius laiko GitHub'e ar kokiam debesyje. Galbūt be interneto neįmanoma apsieit. Grynant konferencijos idėją nuspręsim.</p>
+
+<p><strong>Pavadinimas.</strong> Turim evento pavadinimų porą variantų</p>
+
+<blockquote>
+  <p>No Trolls Allowed</p>
+  
+  <p>Zero Trolls Day</p>
+</blockquote>
+
+<p><strong>Biudžetas.</strong> Į renginį bus pardavinėjami bilietai, taipogi ieškosim įvairių rėmėjų. Kas galėtų paremti pinigais, kas įranga. Kiek dalyvis norėtų ir galėtų mokėti už savaitgalio bilietą? 120-150LTL? Atrodo protinga kaina, net ir skūpam lietuviui.</p>
+
+<p><strong>Laikas.</strong> Galvojam apie rugpjūčio mėnesio antrą pusę ir reikėtų bandyt suderint, kad nebūtų tuo metu jokių festivalių.</p>
+
+<p><strong>Organizavimas.</strong> Konferencija savaime nenutiks - reikia organizuoti. Kas tuo apsiimtų? Tai tikrai ne vieno žmogaus darbas. Smagu būtų padaryti cummunity-based organizavimą, bet kol neturim jokios IT bendruomenės, tai neaišku ko galima tikėtis, tad bandom burti iniciatyvių žmonių grupę, kurie norėtų prisidėti prie tokio renginio organizavimo. Aš prisidedu! Nori prisidėti? Prisijunk prie google group:</p>
+
+<ul>
+<li><a href="https://groups.google.com/forum/?fromgroups#!forum/no-trolls-allowed">No Trolls Allowed</a></li>
+</ul>
