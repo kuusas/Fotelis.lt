3cef52eda9e38a16996ca0774a3b2be7ab90daad
Zilvinas Kuusas <zilvinas@kuusas.lt>
2013-06-23 12:10:08 +0300

    Updated gitignore

---
+++ app/config/config.dist.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+    'db.options' => array(
+        'driver'   => 'pdo_mysql',
+        'host'      => 'localhost',
+        'dbname'    => '',
+        'user'      => '',
+        'password'  => '',
+        'charset'   => 'utf8'
+    ),
+    'swiftmailer.options' = array(
+        'host' => 'host',
+        'port' => '25',
+        'username' => 'username',
+        'password' => 'password',
+        'encryption' => null,
+        'auth_mode' => null
+    ),
+);
